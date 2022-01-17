2951058d86519e9c45586eec645c05fc48b0fb84
Sang Tran <sang.tran1025@gmail.com>
2022-01-17 12:16:39 -0800

    Adjusted prep-javascript-arrays

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -11,5 +11,5 @@ console.log('the 3rd element of the array is ', thirdElement);
 var length = languages.length;
 console.log('The length of the array is ', length);
 
-var lastElement = languages[5];
+var lastElement = languages[languages.length - 1];
 console.log('The last element of the array is ', lastElement);
