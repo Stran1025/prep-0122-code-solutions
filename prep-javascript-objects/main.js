425eb0b8f428cdb011ff6d556b0ad9839260582e
Sang Tran <sang.tran1025@gmail.com>
2022-01-16 21:26:53 -0800

    Completed prep-javascript-objects exercise

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,16 @@
+var person = {
+  firstName: 'Sang ',
+  lastName: 'Tran',
+  hobby: 'Collecting Pokemon Cards',
+  job: 'Code Learner',
+  previousJob: 'T-mobile Expert'
+};
+console.log(person);
+
+console.log('The person\'s name is:', person.firstName + person.lastName);
+
+console.log('The person\'s current job:', person.job);
+
+console.log('The person\'s previous job is:', person.previousJob);
+
+console.log(person);
