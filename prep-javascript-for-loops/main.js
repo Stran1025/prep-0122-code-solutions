16c5b5963850b006d92c0f84f5723c308abcc77b
Sang Tran <sang.tran1025@gmail.com>
2022-01-19 00:39:32 -0800

    Completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+  var twoTime = i * 2;
+  console.log(twoTime);
+}
+
+for (let i = 0; i < 100; i++) {
+  var counter = 100 - i;
+  var countdown = 'Time till explosion ' + counter + '!';
+  console.log(countdown);
+}
